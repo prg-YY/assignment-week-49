842f711e08e4bd3807c32da60bfe4ee54154e379
prg0615 <parguzatt@gmail.com>
2022-01-04 12:53:40 +0100

    Js-version

---
+++ src/Js-version/js.js
@@ -0,0 +1,10 @@
+function openFunction() {
+  document.getElementById("menu").style.width = "200px"
+}
+function closeFunction() {
+  document.getElementById("menu").style.width = "0px"
+}
+
+function toggleBtn() {
+  document.getElementById("popup-1").classList.toggle("active")
+}
